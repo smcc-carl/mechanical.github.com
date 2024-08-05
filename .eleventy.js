fc4426e2e2a09a1154a524b67954e183024b284c
smcc-carl <carl.smcc24@gmail.com>
2024-08-05 09:02:52 +0000

    fixing admin

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
 
     // eleventyConfig.addPassthroughCopy("./src/style.css");
     eleventyConfig.addPassthroughCopy("./src/asset");
+    eleventyConfig.addPassthroughCopy("./src/admin");
 
 
 
